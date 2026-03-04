39dc943c33e83ebcbaa818c7e186c65261a1ab40
shigeno shinji <sazen.shinji@gmail.com>
2026-03-04 10:14:38 +0900

    2026.03.04 10.14(新規作成)

--- src/resources/views/top_page.blade.php
+++ src/resources/views/top_page.blade.php
@@ -10,11 +10,22 @@
 
 <body>
   <div class="topp__content">
-    <h1>「/」にアクセスした時に表示される。</h1>
+    <h1>席替え</h1>
 
-    <p>Controller：TopPageController</p>
-    <p>Action：index</p>
-    <p>Blade file：top_page.blade.php</p>
+    <table border="1">
+      <tr>
+        <th>名前</th>
+        <th>年齢</th>
+      </tr>
+      <tr>
+        <td>佐藤</td>
+        <td>39</td>
+      </tr>
+      <tr>
+        <td>田中</td>
+        <td>20</td>
+      </tr>
+    </table>
 
   </div>
 </body>
